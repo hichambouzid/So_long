4545df9cb0ac954bbaf52abba391c556d0825524
hichambouzid <bouzidhicham51@gmail.com>
2024-01-01 16:35:16 +0100

    push some edits in parcing functions

--- ft_check_map.c
+++ ft_check_map.c
@@ -1,2 +1,60 @@
 #include <unistd.h>
+#include <fcntl.h>
+
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	i++;
+	return (i);
+}
+
+char ft_strdup(char *str)
+{
+	char *ptr;
+	int len;
+
+	len = ft_strlen(str);
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (!ptr)
+	return (NULL);
+	len = 0;
+	while (str[len])
+	{
+		ptr[len] = str[len];
+		len++;
+	}
+	ptr[len] = 0;
+	return (ptr);
+}
+
+char *ft_strjoin(char *s1, char *s2)
+{
+	int i;
+	int j;
+	char *str;
+
+	i = ft_strlen(s1);
+	j = ft_strlen(s2);
+	str = malloc(sizeof(char) * (i + j + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = 0;
+	free(s1);
+	return (str);
+}
 
